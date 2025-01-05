db20094077a62f86c30890eb7f78392d33b45523
SadarulkOfficial <botdeploy179@gmail.com>
2025-01-05 19:39:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ module.exports = {
   BOT_NUMBER: "94771709545",
   PREFIX: ".",
   GOOGLE_API_KEY: "AIzaSyDpCGEwnXmyVURfMCKSA245bNsS2NFewB8",
-  MAX_SIZE: "100", //100MB
+  MAX_SIZE: "3000", // 100MB
+  AUTO_STATUS_READ: "on", // on or off
+  MV_SEND_JID: "120363355439809658@g.us",
 };
